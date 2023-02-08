70fcc7d9d66630468532ff5078dcfa434df7458a
Rhaqim <anusiemj@gmail.com>
2023-02-09 00:03:39 +0100

    generated the GRPC Go files for the numbers and the service, added to the ports

--- internal/ports/framework_left.go
+++ internal/ports/framework_left.go
@@ -2,12 +2,14 @@ package ports
 
 import (
 	"context"
+
+	"github.com/Rhaqim/hexarch/internal/adapters/framework/left/grpc/pb"
 )
 
 type GRPCPort interface {
 	Run()
-	GetAddition(ctx context.Context, a, b int) (int32, error)
-	GetSubtraction(ctx context.Context, a, b int) (int32, error)
-	GetMultiplication(ctx context.Context, a, b int) (int32, error)
-	GetDivision(ctx context.Context, a, b int) (int32, error)
+	GetAddition(ctx context.Context, req *pb.OperationParams) (*pb.Answer, error)
+	GetSubtraction(ctx context.Context, req *pb.OperationParams) (*pb.Answer, error)
+	GetMultiplication(ctx context.Context, req *pb.OperationParams) (*pb.Answer, error)
+	GetDivision(ctx context.Context, req *pb.OperationParams) (*pb.Answer, error)
 }
