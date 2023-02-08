0c42b377753d523fb738908771ed3b16dea2ea62
Rhaqim <anusiemj@gmail.com>
2023-02-08 22:29:56 +0100

    grpc port

--- internal/ports/framework_left.go
+++ internal/ports/framework_left.go
@@ -1,1 +1,13 @@
 package ports
+
+import (
+	"context"
+)
+
+type GRPCPort interface {
+	Run()
+	GetAddition(ctx context.Context, a, b int) (int32, error)
+	GetSubtraction(ctx context.Context, a, b int) (int32, error)
+	GetMultiplication(ctx context.Context, a, b int) (int32, error)
+	GetDivision(ctx context.Context, a, b int) (int32, error)
+}
